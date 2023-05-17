f01939f20720f46e35c07c02bac0a5eae20b6064
nguyenchinhdeptrai <minhanhvoinam@gmail.com>
2023-05-17 21:49:08 +0700

    up screen login and splash

--- Comp/splashScreen .js
+++ Comp/splashScreen .js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View , Image } from 'react-native'
 import React from 'react'
 
 const SplashScreen = ({ navigation }) => {
@@ -8,8 +8,8 @@ const SplashScreen = ({ navigation }) => {
     }, 2000)
   })
   return (
-    <View style={styles}>
-      <Text>Welcome</Text>
+    <View style={styles.view}>
+      <Image source={{ uri: 'https://s3-alpha-sig.figma.com/img/7cf3/1dd8/a0a2203c9eec6ffb1075254cbc52b8b8?Expires=1685318400&Signature=CoieTyPuCH4VQAk~8Twoy~ZSVjKKXYc0tRdwCEbqy4yM9GnS7nSgI4Gr768jTFkC6tFh2W7XHkyqZNQIjKIm15tFCETMZZ751lzFI5huNuC0rJqQns~GcbUSrag--XmnkeoUs9uvzGInxYG8Mc0Au~ZBMn~3ecl76PV1TrDqmuK7IHgyn2I5RcW-9go~ouVrA81pIE-tjrgzgQKyzKemLDd8EHy5aapsMVeuo8Q6bADASbIQWXRx~vMpKjEWLTpw9cwDn8Eue17NEj4fZO2HapvCLEzlgHor8O45YPRCmiLTNoSAQr2KakObmyxrcWePgYBjBlslOfqv-g5NJOJQeQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4' }} style={styles.img} />
     </View>
   )
 }
@@ -17,8 +17,13 @@ const SplashScreen = ({ navigation }) => {
 export default SplashScreen
 
 const styles = StyleSheet.create({
-  flex: 1,
-  backgroundColor: '#fff',
-  alignItems: 'center',
-  justifyContent: 'center',
+  view: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }, img: {
+    width: '100%',
+    height: '100%',
+  }
 })
